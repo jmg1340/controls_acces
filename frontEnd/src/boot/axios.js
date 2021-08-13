5be690a9b4cb2cb36a7d1901b4066bf9f0fcec23
jordi <jordi.miserachs@gmail.com>
2021-08-13 08:11:57 +0200

    commit 2

--- frontEnd/src/boot/axios.js
+++ frontEnd/src/boot/axios.js
@@ -7,7 +7,7 @@ export default ({ Vue }) => {
   /* el servidor esta a PROJECTS/EXPRESS/CONTROLSACCESS*/
   
   Vue.prototype.$axios = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4001',
     withCredentials: false,
     // timeout: 1000,
     // headers: {
